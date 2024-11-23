84aa9c8c0eee076ce82e561cc96aab1a9db16284
Yannik Hahn <yannik@h4hn.de>
2024-11-23 20:05:15 +0100

    feat: add chroma python file

---
+++ m3_sentence_transformer/chroma.py
@@ -0,0 +1,35 @@
+import chromadb
+import pandas as pd
+from FlagEmbedding import BGEM3FlagModel
+from tqdm import tqdm
+
+from explore.funcs import load_datasets
+from m3_sentence_transformer.preprocessing import TextPreprocessor
+
+# Initialize model
+model = BGEM3FlagModel("BAAI/bge-m3", use_fp16=True)
+
+# Initialize ChromaDB client
+chroma_client = chromadb.PersistentClient(path="./db")
+collection = chroma_client.get_or_create_collection(name="neuclir-titles-ru-bge-m3", metadata={"hnsw:space": "cosine"})
+
+# Load datasets
+datasets = load_datasets(["ru"])
+dataset = datasets["ru"]
+docs = pd.DataFrame(dataset.docs_iter())
+
+# Preprocess documents
+#preprocessor = TextPreprocessor("russian")
+docs["title"] = docs["title"].fillna("")
+
+# Split the data into batches
+batches = [docs[i:i+100] for i in range(0, len(docs), 100)]
+
+# Process and add documents to the collection
+for i, batch in enumerate(tqdm(batches)):
+    title_embeddings = model.encode(batch["title"].to_list(), return_dense=True, return_sparse=True, return_colbert_vecs=True)
+    metadata = []
+    for j in range(len(batch)):
+        metadata.append({"url": batch["url"].iloc[j], "text": batch["text"].iloc[j]})
+
+    collection.add(documents=batch["title"].to_list(), embeddings=title_embeddings["dense_vecs"], ids=batch["doc_id"].to_list(), metadatas=metadata)
